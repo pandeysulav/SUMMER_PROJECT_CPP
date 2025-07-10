28816c2de7201c58d33722485d94a1506ebdd39b
Sulav_Pandey <pandeysulav17@gmail.com>
2025-07-10 19:19:51 +0545

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Include the input-output stream library
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Print message to the console
+    return 0;  // Exit the program
+}
